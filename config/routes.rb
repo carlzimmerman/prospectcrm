4d410264805c6a143db16aedaad75a6faef4ba33
Carl Zimmerman <carlpzimmerman@gmail.com>
2018-07-07 23:04:18 -0400

    fixes the routes root path

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   end
 
-  root to: 'peg_groups_path#index'
+  root to: 'peg_groups#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
